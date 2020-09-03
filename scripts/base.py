c02fd3a6aa09a0fff6771e8ff3d4bb0569dacb18
Yuval Adam <_@yuv.al>
2020-09-03 14:08:22 +0300

    Validate feeds before output

--- scripts/base.py
+++ scripts/base.py
@@ -1,5 +1,6 @@
 import toml
 
+from collections import OrderedDict
 from pathlib import Path
 
 
@@ -17,12 +18,32 @@ class GenerateBase:
 
     def get(self):
         """
-        Main method for parsing logic
+        Main method for parsing logic, must return a dictionary of feeds. e.g.
+        {
+            "abc": {
+                "url": "http://example.com/abc.pls"
+            },
+            "def": {
+                "url": "http://example.com/dev.pls"
+                "description": "def feed description"
+            }
+        }
+        For more details see the
         """
         raise NotImplementedError("Subclass must implement feed logic in get()")
 
+    def validate(self, feeds):
+        if not isinstance(feeds, dict):
+            raise Exception("Feeds return type must be a dictionary")
+        for k, v in feeds.items():
+            if not isinstance(v, dict):
+                raise Exception(f"Feed name {k} value must be a dictionary")
+            if not v.get("url"):
+                raise Exception(f"Feed name {k} must have a valid URL")
+
     def generate(self):
         feeds = self.get()
+        self.validate(feeds)
         output = f"{self.NAME}.toml"
         with open(self.lib_path / output, "w") as f:
             toml.dump(feeds, f)
